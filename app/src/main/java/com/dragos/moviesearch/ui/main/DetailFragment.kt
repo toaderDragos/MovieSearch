7c658159e1330235cf6cc069ab4c7814b513c60e
Toader Dragos <toader.dragosh@gmail.com>
2023-11-02 21:51:42 +0200

    Update DetailFragment.kt

--- app/src/main/java/com/dragos/moviesearch/ui/main/DetailFragment.kt
+++ app/src/main/java/com/dragos/moviesearch/ui/main/DetailFragment.kt
@@ -1,5 +1,6 @@
 package com.dragos.moviesearch.ui.main
 
+// Necessary imports
 import android.content.Intent
 import android.net.Uri
 import android.os.Build
@@ -21,33 +22,47 @@ import com.dragos.moviesearch.databinding.FragmentDetailBinding
 import com.dragos.moviesearch.utils.bindImage
 import com.dragos.moviesearch.viewmodel.MainViewModel
 
-
+/**
+ * A Fragment that shows the detailed view of a selected movie.
+ */
 class DetailFragment : Fragment() {
+    // Arguments passed from the navigation component
     private val navigationArgs: DetailFragmentArgs by navArgs()
+    
+    // Movie object that will be initialized with the movie retrieved from the viewModel
     lateinit var movie: Movie
+    
+    // ViewModel that is shared with the activity
     private val viewModel: MainViewModel by activityViewModels()
 
+    /**
+     * Creates and returns the view hierarchy associated with the fragment.
+     */
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        // Inflate the layout for this fragment with data binding
         val binding: FragmentDetailBinding = DataBindingUtil.inflate(
             inflater,
-            com.dragos.moviesearch.R.layout.fragment_detail,
+            R.layout.fragment_detail,
             container,
             false
         )
 
+        // Retrieve and assign the selected movie based on the passed ID
         val id = navigationArgs.selectedMovie
         movie = viewModel.retrieveMovie(id)!!
 
+        // Bind the movie image or set a placeholder if the image is not available
         try {
             bindImage(binding.imageView2, movie.show.image.medium)
         } catch (e: Exception) {
             binding.imageView2.setImageResource(R.drawable.ic_broken_image)
         }
 
+        // Bind the non-null movie details or provide a default value if null
         bindNonNullValue(binding.detailMovieTitle, movie.show.name, "Error")
         bindNonNullValue(binding.detailPremiered, movie.show.premiered, "No date")
         bindNonNullValue(binding.showLenght, movie.show.runTime, "0")
@@ -59,32 +74,28 @@ class DetailFragment : Fragment() {
         bindNonNullValue(binding.detailScore2, movie.show.rating.average, "0.0")
         bindNonNullValue(binding.detailLanguage, movie.show.language, " - ")
 
-        // Taking Html from API as it is - verify null
-        if (movie.show.summary != null) {
-            val unparsedSummary = movie.show.summary
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-                binding.detailSummary.text =
-                    Html.fromHtml(unparsedSummary, Html.FROM_HTML_MODE_LEGACY)
+        // Parse HTML content if available for the movie summary
+        movie.show.summary?.let { summary ->
+            binding.detailSummary.text = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                Html.fromHtml(summary, Html.FROM_HTML_MODE_LEGACY)
             } else {
                 @Suppress("DEPRECATION")
-                binding.detailSummary.text = Html.fromHtml(unparsedSummary)
+                Html.fromHtml(summary)
             }
-        } else {
+        } ?: run {
             binding.detailSummary.text = getString(R.string.no_summary)
         }
 
-        // Some results are incomplete (for example the network branch(with the country name inside) is non- existent in some entries - so we have to put replacements instead
-        if (movie.show.network != null) {
-            binding.detailNetwork.text = movie.show.network.name
-            if (movie.show.network.country.name != null) {
-                binding.detailCountry.text = movie.show.network.country.name
-            } else {
-                binding.detailCountry.text = " - "
-            }
-        } else {
+        // Bind network and country information, use a placeholder if unavailable
+        movie.show.network?.let { network ->
+            binding.detailNetwork.text = network.name
+            binding.detailCountry.text = network.country.name ?: " - "
+        } ?: run {
             binding.detailNetwork.text = " - "
+            binding.detailCountry.text = " - "
         }
 
+        // Set up a listener on the website button to open the movie's official site
         binding.website.setOnClickListener {
             linkButtons(binding.website, movie.show.officialSite)
         }
@@ -92,26 +103,23 @@ class DetailFragment : Fragment() {
         return binding.root
     }
 
+    /**
+     * Binds a non-null value to the given TextView or sets it to a replacement string if null.
+     */
     private fun bindNonNullValue(bindObjText: TextView, movieDetail: String?, replacement: String) {
-        if (movieDetail != null) {
-            bindObjText.text = movieDetail
-        } else {
-            bindObjText.text = replacement
-        }
+        bindObjText.text = movieDetail ?: replacement
     }
 
-    fun linkButtons(button: Button, link: String?) {
-        button.setOnClickListener(object : View.OnClickListener {
-            override fun onClick(v: View?) {
-                if (link != null) {
-                    val url = link
-                    val i = Intent(Intent.ACTION_VIEW)
-                    i.data = Uri.parse(url)
-                    startActivity(i)
-                } else {
-                    Toast.makeText(context, getString(R.string.linkno), Toast.LENGTH_SHORT).show()
-                }
-            }
-        })
+    /**
+     * Attaches a listener to a button that opens a provided URL in a web browser.
+     * Shows a toast if the URL is null.
+     */
+    private fun linkButtons(button: Button, link: String?) {
+        button.setOnClickListener {
+            link?.let {
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(it))
+                startActivity(intent)
+            } ?: Toast.makeText(context, getString(R.string.linkno), Toast.LENGTH_SHORT).show()
+        }
     }
 }
