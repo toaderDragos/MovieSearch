e7d03ba90d44df11c07e741136fda29478f84fbe
Toader Dragos <toader.dragosh@gmail.com>
2023-11-10 11:51:35 +0200

    Update OkHttpSingleton.kt

--- app/src/main/java/com/dragos/moviesearch/network/OkHttpSingleton.kt
+++ app/src/main/java/com/dragos/moviesearch/network/OkHttpSingleton.kt
@@ -2,7 +2,7 @@ package com.dragos.moviesearch.network
 
 import okhttp3.OkHttpClient
 import java.util.concurrent.TimeUnit
-
+// Standard connection to the API
 object OkHttpSingleton {
     val instance: OkHttpClient = OkHttpClient().newBuilder()
         .connectTimeout(ApiConstant.OKHTTP_TIMEOUT, TimeUnit.SECONDS)
