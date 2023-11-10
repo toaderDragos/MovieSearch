ba95770575c81e0680bf58b53f16cd0c0b7d144f
Toader Dragos <toader.dragosh@gmail.com>
2023-11-10 11:50:43 +0200

    Update ApiConstant.kt

--- app/src/main/java/com/dragos/moviesearch/network/ApiConstant.kt
+++ app/src/main/java/com/dragos/moviesearch/network/ApiConstant.kt
@@ -1,5 +1,5 @@
 package com.dragos.moviesearch.network
-
+// For easy retrieval and modifications the constants can be modified from here. 
 object ApiConstant {
     const val MOVIE_URL = "https://api.tvmaze.com/"
     const val SHOW_QUERY_PATH = "/search/shows"
